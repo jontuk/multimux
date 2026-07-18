0bb76424716b2df1b34d14c7c7610178d45a74bb
Jon Turner <jon@jpt.me.uk>
2026-07-18 01:50:07 +0100

    test(web): stub canvas context in jsdom

--- web/src/test-setup.ts
+++ web/src/test-setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+
+// xterm probes canvas support when the app shell mounts. jsdom returns null
+// here but also emits a noisy "not implemented" diagnostic unless the method
+// is supplied by the test environment.
+Object.defineProperty(HTMLCanvasElement.prototype, "getContext", {
+  configurable: true,
+  value: () => null,
+});
